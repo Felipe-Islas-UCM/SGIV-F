52f2de4777bbab7518647653352d1004d4e8e7d8
Felipe-Islas-UCM <147544506+Felipe-Islas-UCM@users.noreply.github.com>
2024-07-02 02:11:32 -0400

    Fix CSS

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './login.css';
+import styles from './LoginPage.module.css';
 import Logo from './comercial_pewen.png';
 
 
 export function LoginPage() {
 
-    //const history = useNavigate();
+    const history = useNavigate();
 
-    //const handleLogin = () => {
+    const handleLogin = () => {
         // Aquí puedes añadir la lógica de autenticación
         // Si la autenticación es exitosa, redirige al dashboard
-        //navigate.push('/admin');
-    //};
+        navigate.push('/admin');
+    };
     return (
-        <div className='Login'>
+        <div className='LoginPage'>
             <html lang="en">
                 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' />
                 <head>
                     <title>Logins</title>
                 </head>
-                <body>
-                    <div class="background"></div>
-                    <div class="card">
-                        <img class="logo" src={Logo} />
+                <body className={styles.body}>
+                    <div className={styles.background}></div>
+                    <div className={styles.card}>
+                        <img className={styles.logo} src={Logo} />
                         <h2><b>Bienvenido!</b></h2>
-                        <form class="form">
+                        <form className={styles.form}>
                             <input type="email" placeholder="Usuario" />
                             <input type="password" placeholder="Contraseña" />
-                            <button>Ingresar</button>
+                            <button onClick={handleLogin}>Ingresar</button>
                         </form>
                         <footer>
                             <b>Olvidaste tu contraseña?</b> Reestablece contraseña
